c0c9de76a95c058883ca638b5deb28401bfa2a6a
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2024-07-17 21:48:27 +0900

    list

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React, {useState} from "react"
 import './App.css'
+import {Todo} from "./model"
 import InputFeild from "./Components/InputFeild"
 
 
 const App: React.FC = () => {
   const [todo, setTodo]=useState<string>("");
+  const [todos, setTodos]=useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, {id:Date.now(), todo, isDone:false}])
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
 
   return <div className="App">
     <span className="heading">Taskify</span>
-    <InputFeild />
+    <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+    {/*<TodoList />*/}
+    {todos.map((t)=>(
+      <li>{t.todo}</li>
+    ))}
   </div>
 }
-//27분 38초!
+
 export default App
+//37:54
--- src/Components/InputFeild.tsx
+++ src/Components/InputFeild.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./styles.css";
 
-const InputFeild = () => {
-    return (
-        <form className='input'>
-            <input type="input" placeholder="Enter task" className="input__box"/>
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd:(e: React.FormEvent)=>void; //아무것도 반환하지 않는 함수 의미
+}
+
+const InputFeild: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+    const inputRef=useRef<HTMLInputElement>(null);
+
+    return ( //제출을 누를 때마다 handleAdd 함수 활성화
+        <form className='input' onSubmit={(e)=>{
+        handleAdd(e);
+        inputRef.current?.blur();
+        }}> 
+            <input 
+                ref={inputRef}
+                type="input" 
+                placeholder="Enter task" 
+                value={todo}
+                onChange={(e)=>setTodo(e.target.value)}
+                className="input__box"/>
             <button className='input_submit' type="submit">
                 Go
             </button>
         </form>
     )
 }
-
+//37:54
 export default InputFeild
